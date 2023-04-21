44234a8b6bec2d32b0ebdd1638d65ae27ebf9b45
Friedrich Tschirpke <friedrich.tschirpke@student.hu-berlin.de>
2023-04-21 17:43:18 +0200

    feat: launcher updates seem to work now

--- scenes/starter.gd
+++ scenes/starter.gd
@@ -19,6 +19,12 @@ const TWENTY_MINUTES: float = 1200
 # WINDOW DRAGGING
 var drag_position = null
 
+# MORE INFORMATION
+var release_notes: String = ""
+var asset_name: String = ""
+var asset_url: String = ""
+var asset_size: int = 0
+
 func _on_draggable_header_gui_input(event: InputEvent) -> void:
     if event is InputEventMouseButton and event.button_index == 1:
         if event.is_pressed():
@@ -56,7 +62,7 @@ func save_version() -> void:
 func _ready() -> void:
     var start_timestamp: float = Time.get_unix_time_from_system()
     var look_for_updates: bool = start_timestamp - last_fetched_timestamp >= TWENTY_MINUTES
-#    look_for_updates = false # TODO - only testing
+    look_for_updates = true # TODO - only testing
     if look_for_updates:
         print("FETCHING LAUNCHER UPDATES")
         look_for_launcher_updates()
@@ -65,15 +71,28 @@ func _ready() -> void:
         print("SKIPPING LAUNCHER UPDATES")
         start_launcher()
 
-func make_http_request(function_on_completion: Callable, request_url: String, download_file_path: String = "") -> HTTPRequest:
+func make_http_request(function_on_completion: Callable, request_url: String) -> HTTPRequest:
     var http_request: HTTPRequest = HTTPRequest.new()
     add_child(http_request)
     http_request.set_tls_options(TLSOptions.client())
     http_request.request_completed.connect(function_on_completion)
-    if download_file_path != "":
-        http_request.download_file = download_file_path
-    http_request.request(request_url)
+    http_request.request(request_url, ["Accept: application/vnd.github+json"])
     return http_request
+
+func download_file(url: String, file_path: String, _on_download_finished: Callable) -> void:
+    if not FileAccess.file_exists(file_path):
+        var file: FileAccess = FileAccess.open(file_path, FileAccess.WRITE)
+        if not file:
+            push_error("Error while trying to open file %s: %s" % [file_path, FileAccess.get_open_error()])
+        else:
+            file.close()
+
+    var http_request: HTTPRequest = HTTPRequest.new()
+    add_child(http_request)
+    http_request.set_tls_options(TLSOptions.client())
+    http_request.download_file = file_path
+    http_request.request_completed.connect(_on_download_finished)
+    http_request.request(url, ["Accept: application/octet-stream"])
 
 func look_for_launcher_updates() -> void:
     label.text = "Looking for launcher updates..."
@@ -83,14 +102,23 @@ func _on_launcher_update_request_completed(result: int, response_code: int, head
     if result != HTTPRequest.RESULT_SUCCESS:
         push_error("HTTP request to get launcher update was not successful")
         return
-    print(JSON.stringify(JSON.parse_string(body.get_string_from_utf8()), "    "))
-    print(JSON.stringify(headers, "    "))
     var json: Array = JSON.parse_string(body.get_string_from_utf8())
     if len(json) == 0:
         push_error("Could not find any Launcher releases.")
         return
     var last_release: Dictionary = json[0]
-    save_version()
+    
+    release = last_release.get("name", "")
+    release_notes = last_release.get("body", "")
+    
+    var release_assets: Array = last_release.get("assets", [])
+    for asset_dict in release_assets:
+        asset_name = asset_dict.get("name", "")
+        if asset_name.ends_with(".pck"):
+            asset_url = asset_dict.get("url", "")
+            asset_size = asset_dict.get("size", -1)
+            download_launcher_pck(asset_url)
+            break
 
 func download_launcher_pck(download_url: String) -> void:
     if not FileAccess.file_exists(launcher_pck_path):
@@ -99,16 +127,18 @@ func download_launcher_pck(download_url: String) -> void:
             push_error(FileAccess.get_open_error())
         else:
             file.close()
-    make_http_request(_on_launcher_pck_downloaded, download_url, launcher_pck_path)
+    download_file(download_url, launcher_pck_path, _on_launcher_pck_downloaded)
 
 func _on_launcher_pck_downloaded(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
     if result != HTTPRequest.RESULT_SUCCESS:
         push_error("HTTP Request to download file was not successful")
+    save_version()
     start_launcher()
 
 func start_launcher() -> void:
     if FileAccess.file_exists(launcher_pck_path):
         var successful = ProjectSettings.load_resource_pack(launcher_pck_path)
+        print("LOADED")
         if not successful:
             push_error("Failed to load the launcher update. Continuing with the base version...")
     scene_tree.change_scene_to_file(launcher_scene_path)
